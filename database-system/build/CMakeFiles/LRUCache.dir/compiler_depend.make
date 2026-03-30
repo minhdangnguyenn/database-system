5c6e89630760128b6ef8fdfa03717c94d066743d
Minh Nguyen <51837721+minhdangnguyenn@users.noreply.github.com>
2026-03-30 21:46:32 +0200

    Delete database-system/build directory

--- database-system/build/CMakeFiles/LRUCache.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for LRUCache.
-# This may be replaced when dependencies are built.
--- database-system/build/CMakeFiles/LRUCache.dir/page.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/LRUCache.dir/page.cpp.o: \
- /home/minhnguyen/repo/LRU-Replacement-Policy/database-system/page.cpp \
- /usr/include/stdc-predef.h \
- /home/minhnguyen/repo/LRU-Replacement-Policy/database-system/page.h
