29644f6bc09ddb9a977284b0eea722e7137dec02
Minh Nguyen <dangminh214@gmail.com>
2026-04-05 17:05:26 +0200

    delete comments

--- bplus-tree/src/page.cpp
+++ bplus-tree/src/page.cpp
@@ -1,7 +1,5 @@
 #include "../include/page.h"
 
-// int Page::getKey() const { return this->key; }
-// int Page::getValue() const { return this->value; }
 void Page::set_value(int val) {this->value = val;}
 void Page::set_key(int key) {this->key = key;}
 char* Page::get_data() { return this->data; }
