69f9883eaeea09541e4c5df551424346a6b85bfe
Minh Nguyen <dangminh214@gmail.com>
2026-07-26 21:13:21 +0200

    Update main.cpp

--- cpp-practice/map/main.cpp
+++ cpp-practice/map/main.cpp
@@ -5,14 +5,17 @@
 
 int main() {
     std::map<std::string, int> ordered_dict;
+    std::map<int, int> dict2;
     std::unordered_map<std::string, int> unordered_dict;
 
     // Inserting elements
     ordered_dict["apple"] = 5;
+    dict2[1] = 10;
     unordered_dict["banana"] = 3;
 
     // Accessing elements
     std::cout << "Ordered: " << ordered_dict["apple"] << std::endl;
+    std::cout << "Dict2: " << dict2[1] << std::endl;
     std::cout << "Unordered: " << unordered_dict["banana"] << std::endl;
 
     return 0;
